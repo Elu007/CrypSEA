728123da06c65e9e6e03dd45332cadacc5854522
Elaf <skelaf2000@gmail.com>
2023-01-19 21:08:03 +0530

    Added the coinlist and changes in navbar

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,16 @@
 import React from 'react'
+import Navbar from './Navbar'
+import CoinsTable from './CoinsTable'
 
 const Home = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <>
+            <Navbar />
+            <div className='container'>
+                <CoinsTable />
+            </div>
+        </>
+    )
 }
 
 export default Home
-
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,17 @@
+import { MenuItem, Select } from '@mui/material'
 import React from 'react'
-import logo from "./Assets/logo.jpg"
+import { CryptoState } from '../CryptoContext'
+import logo from "./Assets/logo.png"
 import './Navbar.css'
 
 const Navbar = () => {
+
+    const { currency, setCurrency } = CryptoState();
     return (
         <div>
             <nav className="navbar navbar-expand-lg bg-body-tertiary">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="/home"><img src={logo} alt="logo" width="100px" height="54px" /></a>
+                    <a className="navbar-brand" href="/home"><img src={logo} alt="logo" width="100px" height="48px" /></a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
@@ -40,13 +44,13 @@ const Navbar = () => {
                         </ul>
                     </div>
                 </div>
+                <Select variant='outlined' value={currency} onChange={(e) => setCurrency(e.target.value)} style={{width:100, height: 40, marginRight: 15}}>
+                    <MenuItem value={"USD"}>USD</MenuItem>
+                    <MenuItem value={"INR"}>INR</MenuItem>
+                </Select>
             </nav>
         </div>
     )
 }
 
 export default Navbar
-
-
-
-
