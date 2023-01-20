b94fc39c57d5fc9a7a4ef6f8839e45cc224492e9
Elaf <skelaf2000@gmail.com>
2023-01-20 21:40:28 +0530

    Version 1.0.2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css'
+import Footer from './components/Footer';
 import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
      <Home/>
+     <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='container mt-5'>
+      <div className='text-center'>
+        CrypSEA &copy; 2023
+        <p>This website is under development</p>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
