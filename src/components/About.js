f5b27e5579b38eeab1c0c9e35a95987ff4e70bc7
Elaf <skelaf2000@gmail.com>
2023-03-06 20:31:10 +0530

    Version 1.5.1

--- src/components/About.js
+++ src/components/About.js
@@ -5,9 +5,11 @@ import profile from './Assets/profile.png'
 const About = () => {
   return (
     <div className='container text-center'>
-      <div className='d-flex justify-content-around mt-4 shadow-lg p-3 mb-5 bg-body rounded'>
-        <img className='rounded-4' src={about} alt="about" />
-        <div>
+      <div className='row mt-4 shadow-lg p-3 mb-5 bg-body rounded'>
+        <div className="col">
+          <img className='rounded-4' src={about} style={{ width: "40vw", height: "70vh" }} alt="about" />
+        </div>
+        <div className='col'>
           <h1 className='mt-5'>About CrypSEA</h1>
           <p className='p-5 text-start'>We are developing a financial platform that will benefit cryptocurrency investors by being more fair, accessible, efficient, and transparent.
             <br />
@@ -15,21 +17,17 @@ const About = () => {
         </div>
       </div>
       <hr style={{ border: "2px solid" }} />
-      <div className='mt-3 shadow-lg p-3 mb-5 bg-body rounded'>
+      <div className='mt-5 shadow-lg p-3 mb-5 bg-body rounded'>
         <h1>Our Executive Team</h1>
-        <div className='d-flex justify-content-around mt-4'>
-          <div>
-            <img className='p-3 rounded-5' src={profile} alt="profile" />
-            <h1>Sk Elaf Ahmed</h1>
-            <h5>Founder & CEO of CrypSEA</h5>
-          </div>
-          <div className='w-50' style={{marginTop:"25vh"}}>
-            <p>
+        <div className='row'>
+          <img className='mx-auto p-3 rounded-5' style={{height:"65vh", width:"35vw"}} src={profile} alt="profile" />
+          <h1>Sk Elaf Ahmed</h1>
+          <h5>Founder & CEO of CrypSEA</h5>
+          <p className='mt-3'>
             As a software engineer, I adore creating online applications. Making investment social and accessible to everyone is my aim.
-            <br/>
-We shall succeed in our aim by working hard and with passion.
-            </p>
-          </div>
+            <br />
+            We shall succeed in our aim by working hard and with passion.
+          </p>
         </div>
       </div>
     </div>
